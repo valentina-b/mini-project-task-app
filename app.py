e867e82be1623dc5239e31ebb964a12d6f00fc67
valentina-b <valentina.bedi@gmail.com>
2020-06-12 19:03:47 +0000

    Add task category selection

--- app.py
+++ app.py
@@ -22,12 +22,14 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html",
+                            tasks=mongo.db.tasks.find())
 
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html')
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
 
 
 if __name__ == '__main__':
